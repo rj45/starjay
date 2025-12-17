f53f51e470c2bff0ac8c749c81c1b6907e896c00
Ryan rj45 Sanche <ryan@rj45.ca>
2025-12-17 08:56:26 -0800

    Switch to sdl3 backend as it loads faster / is less glitchy

--- build.zig
+++ build.zig
@@ -7,7 +7,7 @@ pub fn build(b: *std.Build) void {
     const dvui_dep = b.dependency("dvui", .{
         .target = target,
         .optimize = optimize,
-        .backend = .sdl3gpu,
+        .backend = .sdl3,
     });
 
     const clap = b.dependency("clap", .{
@@ -22,7 +22,7 @@ pub fn build(b: *std.Build) void {
             .target = target,
             .optimize = optimize,
             .imports = &.{
-                .{ .name = "dvui", .module = dvui_dep.module("dvui_sdl3gpu") },
+                .{ .name = "dvui", .module = dvui_dep.module("dvui_sdl3") },
                 .{ .name = "clap", .module = clap.module("clap") },
             },
         }),
