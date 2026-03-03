f925fa634231e6b3e829038ef07a8a82e6a478a5
Ryan rj45 Sanche <ryan@rj45.ca>
2026-03-03 12:33:37 -0800

    Add testing to sjtilemap

--- tools/sjtilemap/build.zig
+++ tools/sjtilemap/build.zig
@@ -42,4 +42,13 @@ pub fn build(b: *std.Build) void {
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
+
+    const exe_tests = b.addTest(.{
+        .root_module = exe_mod,
+    });
+
+    const run_exe_tests = b.addRunArtifact(exe_tests);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_exe_tests.step);
 }
